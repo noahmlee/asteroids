18b3624bb750bb0ad70b65ab10604e30c74a95b9
Noah Lee <noahsaures@gmail.com>
2026-04-14 14:21:55 -0400

    Add delta time clock and lock game at 60 fps

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from logger import log_state
 
 def main():
     pygame.init()
-    
+        
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    dt = 0
+
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
